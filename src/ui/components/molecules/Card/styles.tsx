d2e2290d312b1a8d43498b721061214409e7cc4d
Thiago Kasper de Souza <thiagokasper101@gmail.com>
2022-08-26 18:03:43 -0300

    #9-release: componente card do dashboard

---
+++ src/ui/components/molecules/Card/styles.tsx
@@ -0,0 +1,35 @@
+import { styled } from "@mui/system";
+
+export const StyledContainer = styled('div')`
+    min-width: 30%;
+    min-height: 180px;
+    border-radius: 10px;
+    padding: 1.4em;
+`;
+
+export const TitleContainer = styled('div')`
+    font-style: normal;
+    font-weight: 700;
+    font-size: 20px;
+    line-height: 23px;
+    color: #FFFFFF;
+`;
+
+export const AmountContainer = styled('div')`
+    font-style: normal;
+    font-weight: 700;
+    font-size: 30px;
+    line-height: 33px;
+    color: #FFFFFF;
+`;
+
+export const Icon = styled('img')``;
+
+export const ObsContainer = styled('div')`
+    margin-top: 5em;
+    font-style: normal;
+    font-weight: 400;
+    font-size: 12px;
+    line-height: 14px;
+    color: #FFFFFF;
+`;
